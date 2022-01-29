ec0645a990b68ddcd80de22cee3ef08bbf1b49f9
rmateluna <rodrigo_mateluna@msn.com>
2022-01-29 11:19:03 -0300

    cambio ejecucion.groovy

--- vars/ejecucion.groovy
+++ vars/ejecucion.groovy
@@ -8,37 +8,39 @@
 */
 
 def call(){
+    pipeline {
+        agent any
+        environment {
+            NEXUS_USER      = credentials('user-nexus')
+            NEXUS_PASSWORD  = credentials('password-nexus')
+        }
+        parameters {
+            choice(
+                name:'compileTool',
+                choices: ['Maven', 'Gradle'],
+                description: 'Seleccione herramienta de compilacion'
+            )
+        }
+        stages {
+            stage('pipeline') {
+                steps {
+                    script {
 
-  pipeline {
-    agent any
-    environment {
-        NEXUS_USER      = credentials('user-nexus')
-        NEXUS_PASSWORD  = credentials('password-nexus')
-    }
-    parameters {
-        choice(
-            name:'compileTool',
-            choices: ['Maven', 'Gradle'],
-            description: 'Seleccione herramienta de compilacion'
-        )
-    }
-    stages {
-        stage('pipeline') {
-            steps {
-                script {
+                        params.compileTool
 
-                    params.compileTool
-
-                    switch(params.compileTool)
-                    {
-                        case 'Maven':
-                            def ejecucion = load 'maven.groovy'
-                            ejecucion.call()
-                        break;
-                        case 'Gradle':
-                            def ejecucion = load 'gradle.groovy'
-                            ejecucion.call()
-                        break;
+                        switch(params.compileTool)
+                        {
+                            case 'Maven':
+                                //def ejecucion = load 'maven.groovy'
+                                //ejecucion.call()
+                                maven.call()
+                            break;
+                            case 'Gradle':
+                                //def ejecucion = load 'gradle.groovy'
+                                //ejecucion.call()
+                                gradle.call()
+                            break;
+                        }
                     }
                 }
             }
